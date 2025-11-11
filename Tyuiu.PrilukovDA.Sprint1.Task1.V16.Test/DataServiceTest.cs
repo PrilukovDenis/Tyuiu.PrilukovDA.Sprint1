eb01856475663f52e6a08a48490bedaaaa5c97f4
deusvela <d.prilukov@aerokod.ru>
2025-11-11 23:02:36 +0500

    Task1

---
+++ Tyuiu.PrilukovDA.Sprint1.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task1.V16.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint1.Task1.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            double x = 1;
+            double y = 2;
+            double a = 3;
+            Assert.AreEqual(16, dataService.Calculate(x, y, a));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint1.Task1.V16/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task1.V16.Lib;
+
+DataService data = new DataService();
+
+Console.Write("Введите первое значение: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе значение: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите третье значение: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(data.Calculate(x, y, a));
+Console.ReadKey();
