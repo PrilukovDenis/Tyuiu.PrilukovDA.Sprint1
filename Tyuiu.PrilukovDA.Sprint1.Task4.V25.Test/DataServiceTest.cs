a1d71487c79cefc55f6478de4e45549e002aa31e
deusvela <d.prilukov@aerokod.ru>
2025-11-12 00:11:21 +0500

    Task4

---
+++ Tyuiu.PrilukovDA.Sprint1.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task4.V25.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint1.Task4.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double alpha = 1.5;
+            Assert.AreEqual(0.934, ds.Calculate(alpha));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint1.Task4.V25/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task4.V25.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("Введите градусы для подсчета по формуле");
+double alpha = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(ds.Calculate(alpha));
+Console.ReadKey();
+
+
