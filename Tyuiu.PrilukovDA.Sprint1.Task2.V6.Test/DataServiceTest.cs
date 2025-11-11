a46d0487aa1a4da2948d1cf34710892e6788cf9e
deusvela <d.prilukov@aerokod.ru>
2025-11-11 23:25:30 +0500

    Task2

---
+++ Tyuiu.PrilukovDA.Sprint1.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task2.V6.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint1.Task2.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 1000;
+
+            Assert.AreEqual(1, ds.ConvertMToKm(x));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint1.Task2.V6/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task2.V6.Lib;
+
+DataService ds = new DataService();
+
+Console.Write("Введите расстояние в метрах ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ds.ConvertMToKm(x));
+Console.ReadKey();
