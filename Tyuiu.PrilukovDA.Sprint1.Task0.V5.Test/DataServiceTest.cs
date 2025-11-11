4fe904592e71b185281819e0e851b5441aa4f4a5
deusvela <d.prilukov@aerokod.ru>
2025-11-11 22:24:11 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.PrilukovDA.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint1.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            Assert.AreEqual(12, dataService.Calculate());
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint1.Task0.V5/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.PrilukovDA.Sprint1.Task0.V5.Lib;
+
+DataService calc = new DataService();
+
+Console.WriteLine(calc.Calculate());
+Console.ReadKey();
