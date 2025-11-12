6b78973756c57c8cf4bdecd95cf3466a3c9b84e1
deusvela <d.prilukov@aerokod.ru>
2025-11-12 22:55:59 +0500

    Task7

---
+++ Tyuiu.PrilukovDA.Sprint1.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task7.V5.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint1.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(-0.545, ds.Calculate(2));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint1.Task7.V5/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task7.V5.Lib;
+Console.Title = "Спринт #1 | Выполнил: Прилуков Д.А. | НТм-24-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #5                                                              *");
+Console.WriteLine("* Выполнил: Иванов Евгений Олегович | ПИНб-25-1                           *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("*Написать консольную программу на C#                                      *");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("*                                                                         *");
+
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("***********         ln|cos(x)|                                             *");
+Console.WriteLine("*********** z =   ---------------                                          *");
+Console.WriteLine("***********         ln(1 + x^2)                                            *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите x");
+double x = Convert.ToDouble(Console.ReadLine());
+DataService ds = new DataService();
+
+Console.WriteLine("* ВЫВОД:                                                        *");
+Console.WriteLine(ds.Calculate(x));
+Console.ReadLine();
