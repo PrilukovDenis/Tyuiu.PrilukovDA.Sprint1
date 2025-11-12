a9667285917e2e2cc3608545627999641022c837
deusvela <d.prilukov@aerokod.ru>
2025-11-12 21:56:57 +0500

    Task5

---
+++ Tyuiu.PrilukovDA.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(-18, ds.FahrenheitToСelsius(0));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.PrilukovDA.Sprint1.Task5.V2.Lib;
+
+double temp = Convert.ToDouble(Console.ReadLine());
+DataService ds = new DataService();
+
+Console.WriteLine(ds.FahrenheitToСelsius(temp));
+Console.ReadKey();
